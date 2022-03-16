352e043047a39b038c50550be75123ad9eb66e8a
Nomoki <sroravitoak@hotmail.com>
2022-03-16 19:56:34 +0700

    font size update
    form page head font size update

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -77,7 +77,7 @@ const Form = ({ currentId, setCurrentId }) => {
   return (
     <Paper className={classes.paper}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Workspace'}</Typography>
+        <Typography variant="h4">{currentId ? `Editing "${post.title}"` : 'Creating a Workspace'}</Typography>
         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
