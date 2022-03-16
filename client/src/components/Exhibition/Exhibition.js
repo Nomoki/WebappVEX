c15557ff2fbed529fd5e73234c507ecd2b1b4784
Puthinart-S <boatkakkakmak@gmail.com>
2022-03-17 01:52:04 +0700

    แก้หน้า ex

--- client/src/components/Exhibition/Exhibition.js
+++ client/src/components/Exhibition/Exhibition.js
@@ -68,11 +68,12 @@ const Exhibition = () => {
       <directionalLight position={[10, 10, 5]} intensity={2} />
       <directionalLight position={[-10, -10, -5]} intensity={1} />
 
-      {/* {transforms.map((trans) => (
+      {transforms.map((trans) => (
+        trans.Objnum === 1 ?
         <Model url="/kajard.glb" sx={trans.ScaleX} sy={trans.ScaleY} sz={trans.ScaleZ}  px={trans.TransX} py={trans.TransY} pz={trans.TransZ} rx={trans.RotateX} ry={trans.RotateY} rz={trans.RotateZ} key={trans._id} />
-      ))} */}
-
-      {/* <Model url="/kajard.glb" sx={1} sy={1} sz={1}  px={0} py={0} pz={0} rx={0} ry={0} rz={0} /> */}
+        : 
+        <Model url="/morn.glb" sx={trans.ScaleX} sy={trans.ScaleY} sz={trans.ScaleZ}  px={trans.TransX} py={trans.TransY} pz={trans.TransZ} rx={trans.RotateX} ry={trans.RotateY} rz={trans.RotateZ} key={trans._id} />
+        ))}
 
       {target && <TransformControls object={target} mode={mode} />}
       <OrbitControls makeDefault />
