ce14f490e252a9910ac4e3cdc02d883d035a08f9
Puthinart-S <boatkakkakmak@gmail.com>
2022-07-14 21:48:32 +0700

    deploy stuff

--- server/index.js
+++ server/index.js
@@ -19,10 +19,14 @@ app.use(express.json({ limit: '1000mb', extended: true }))
 app.use(express.urlencoded({ limit: '1000mb', extended: true }))
 app.use(cors());
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 app.use("/user", userRoutes);
 app.use("/pos", posRoutes);
 app.use("/posprod", posproductRoutes);
+
+app.get("/", (req, res) => {
+  res.send("WebappVEX_API")
+});
 
 const PORT = process.env.PORT || 5000;
 
